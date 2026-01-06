ac5ca4acb03a93e9a46e6a23f5bd072dc1dc7e5f
Rohit_Kumar <kumar25rohit08@gmail.com>
2026-01-06 08:49:10 +0530

    Related products and Add to cart function

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,23 +1,55 @@
-import { createContext } from "react"; 
-import { products } from "../assets/assets"
+import { createContext, useEffect, useState } from "react";
+import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-
-  const currency = '$';
+  const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
 
+  const addToCart = async (itemID, size) => {
+
+    if(!size){
+      toast.error('Select product size')
+      return;
+    }
+
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemID]) {
+      if (cartData[itemID][size]) cartData[itemID][size] += 1;
+      else cartData[itemID][size] = 1;
+    } else {
+      cartData[itemID] = {};
+      cartData[itemID][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
   const value = {
-    products, currency, delivery_fee
-  }
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItems,
+    addToCart,
+  };
 
-  return(
-    <ShopContext.Provider value={value}>
-      {props.children}
-    </ShopContext.Provider>
-  )
-}
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
