8f7f44dc9eac46facac5e6a0e96b098ffc30c021
Rohit_Kumar <kumar25rohit08@gmail.com>
2026-01-08 11:01:41 +0530

    Cart Calculations added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -10,6 +11,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
   const addToCart = async (itemID, size) => {
 
@@ -31,6 +33,40 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
+  const getCartCount = () =>{
+    let totalCount = 0;
+    for(const productId in cartItems){
+      for(const size in cartItems[productId]){
+        const qty = cartItems[productId][size];
+        if(typeof qty === 'number' && qty > 0)
+            totalCount += qty;
+      }
+    }
+
+    return totalCount;
+  }
+
+  const totalCartAmount = () => {
+    let totalAmount = 0;
+    for(const productId in cartItems){
+        let productInfo = products.find((product)=> product._id === productId)
+      for(const size in cartItems[productId]){
+        const qty = cartItems[productId][size];
+        if(typeof qty === 'number' && qty > 0){
+          totalAmount += qty * productInfo.price;
+        }
+      }
+    }
+
+    return totalAmount;
+  }
+
+  const updateQuantity = (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  }
+
   useEffect(() => {
     console.log(cartItems);
   }, [cartItems]);
@@ -45,6 +81,10 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
+    getCartCount,
+    updateQuantity,
+    totalCartAmount,
+    navigate
   };
 
   return (
