ce26885cb13060cd0a8527a5d059427a442ea713
Rohit_Kumar <kumar25rohit08@gmail.com>
2026-03-07 23:48:19 +0530

    adding products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,53 @@
+import {v2 as cloudinary} from "cloudinary";
+import productModel from "../models/productModel.js";
+
+
 //function for add product
 const addProduct = async (req, res) => {
+  try {
+    const {name, description, price, category, subCategory, sizes, bestseller} = req.body;
 
-}
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    // const image1 = req.files?.image1?.[0];
+    // const image2 = req.files?.image2?.[0];
+    // const image3 = req.files?.image3?.[0];
+    // const image4 = req.files?.image4?.[0];
+    const images = [image1, image2, image3, image4].filter((item)=> item !== undefined);
+
+    let imagesUrl = await Promise.all(images.map(async (item) => {
+      const result = await cloudinary.uploader.upload(item.path, {
+        resource_type: "image"
+      });
+      return result.secure_url;
+    }));
+
+    const productData = {
+      name,
+      description,
+      price: Number(price),
+      category,
+      subCategory,
+      sizes: JSON.parse(sizes),
+      bestSeller: bestseller === "true" ? true : false,
+      image: imagesUrl,
+      date: Date.now()
+    };
+
+    console.log(productData);
+
+    const newProduct = new productModel(productData);
+    await newProduct.save();
+
+    res.json({success: true, message: "Product added successfully!"})
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: error.message});
+  }
+} 
 
 //function for remove product
 const removeProduct = async (req, res) => {
